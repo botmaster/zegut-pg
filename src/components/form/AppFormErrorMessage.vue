241b5855e6f7af983a97e8ac7b432ba6bb3867ea
botmaster <pascal.achard@gmail.com>
2023-05-24 12:41:01 +0200

    refactor: Create inputs components

---
+++ src/components/form/AppFormErrorMessage.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+defineProps<{
+  id: string | number
+}>()
+</script>
+
+<template>
+  <p aria-live="assertive" class="text-sm text-red-700" :id="id">
+    <slot />
+  </p>
+</template>
+
+<style scoped></style>
---
+++ src/components/form/AppInput.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import AppFormErrorMessage from '@/components/form/AppFormErrorMessage.vue'
+import UniqueID from '../../utils/uuid'
+
+defineProps<{
+  modelValue: string | number
+  label?: string
+  error?: string
+}>()
+
+const emit = defineEmits<{
+  (event: 'update:modelValue', payload: string | number): void
+}>()
+
+const uuid = UniqueID().getID()
+
+const updateValue = (event) => {
+  let val = event.target.value
+  emit('update:modelValue', val)
+}
+</script>
+
+<template>
+  <label v-if="label" :for="uuid" class="font-medium">
+    {{ label }}
+  </label>
+  <input
+    class="form-input"
+    v-bind="{
+      ...$attrs,
+      onInput: updateValue
+    }"
+    :id="uuid"
+    :value="modelValue"
+    :placeholder="label"
+    :aria-describedby="error ? `${uuid}-error` : null"
+    :aria-invalid="!!error"
+    :class="{ error }"
+  />
+
+  <AppFormErrorMessage v-if="error" :id="`${uuid}-error`">
+    {{ error }}
+  </AppFormErrorMessage>
+</template>
+
+<style scoped></style>
