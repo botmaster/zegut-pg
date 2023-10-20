7428af9ed1c9bb7c79b57fe720a03df08a7d48fb
botmaster <pascal.achard@gmail.com>
2023-10-20 10:10:17 +0200

    feat(deps): Add Testing

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,13 +1,16 @@
 import { fileURLToPath } from 'node:url'
-import { mergeConfig } from 'vite'
-import { configDefaults, defineConfig } from 'vitest/config'
+import { mergeConfig, defineConfig, configDefaults } from 'vitest/config'
 import viteConfig from './vite.config'
 
 export default mergeConfig(
   viteConfig,
   defineConfig({
     test: {
-      environment: 'jsdom',
+      // active les API compatibles avec jest globalement
+      globals: true,
+      // simule le DOM avec happy-dom
+      // (requiert l'installation de happy-dom en dépendance additionnelle)
+      environment: 'happy-dom',
       exclude: [...configDefaults.exclude, 'e2e/*'],
       root: fileURLToPath(new URL('./', import.meta.url))
     }
